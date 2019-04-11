6938797e4aed9d223c80f95abd3c8521ad533060
Muqiu7890 <1337651840@qq.com>
2019-04-11 09:43:54 +0800

    add employee

--- src/main/java/com/example/employee/restfulapi/Service/EmployeeService.java
+++ src/main/java/com/example/employee/restfulapi/Service/EmployeeService.java
@@ -1,4 +1,42 @@
 package com.example.employee.restfulapi.Service;
 
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List findEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee findEmployeeById(Long employeeId) {
+        return employeeRepository.findOne(employeeId);
+    }
+
+    public List<Employee> findEmployeeByMale() {
+        return employeeRepository.findByGender("male");
+    }
+
+    public Page<Employee> findEmployeesByPageByPageSize(int page, int pageSize) {
+        return employeeRepository.findAll(new PageRequest(page-1,pageSize));
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long employeeId) {
+        employeeRepository.delete(employeeId);
+    }
+
 }
--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,5 +1,57 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.Service.EmployeeService;
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("")
 public class EmployeeController {
     //在此处完成Employee API
+    @Autowired
+    private EmployeeService employeeService;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public ResponseEntity findEmployees() {
+        return new ResponseEntity<>(employeeService.findEmployees(), HttpStatus.OK);
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public ResponseEntity findEmployeeById(@PathVariable Long employeeId) {
+        return new ResponseEntity<>(employeeService.findEmployeeById(employeeId),HttpStatus.OK);
+    }
+
+    @GetMapping("/employees/page/{page}/pageSize/{pageSize}")
+    public ResponseEntity findEmployeesByPageByPageSize(@PathVariable int page,@PathVariable int pageSize) {
+        return new ResponseEntity<>(employeeService.findEmployeesByPageByPageSize(page,pageSize),HttpStatus.OK);
+    }
+
+    @GetMapping("/employees/male")
+    public ResponseEntity findEmployeeByMale() {
+        return new ResponseEntity<>(employeeService.findEmployeeByMale(),HttpStatus.OK);
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity addEmployee(@RequestBody Employee employee) {
+        employeeService.addEmployee(employee);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/employees/{employeeId}")
+    public ResponseEntity updateEmployee(@RequestBody Employee employee) {
+        employeeRepository.save(employee);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public ResponseEntity deleteEmployee(@PathVariable Long employeeId) {
+        employeeService.deleteEmployee(employeeId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
